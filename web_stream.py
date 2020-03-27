099ccacc0d58a67cb5c0a987ad8211323523e993
TheButcherOfBlaviken <nabiladnan99@gmail.com>
2020-03-28 03:45:48 +0600

    data file location

--- web_stream.py
+++ web_stream.py
@@ -6,7 +6,7 @@ import plotly_express as px
 
 # @st.cache
 def read_data():
-    data = pd.read_csv("combined_csv.csv")
+    data = pd.read_csv("https://raw.githubusercontent.com/TheButcherOfBlaviken/epldata/master/combined_csv.csv")
     # print(data.shape)
     # print(data.columns)
     # print(data.head(10))
